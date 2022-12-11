07373b904952d3bdb524c805bb4de59a4a0fbd8b
Alexey Kunitsky <kunitsky.alexey@gmail.com>
2022-12-11 22:40:12 +1000

    Add loading prompt

--- src/system/App.js
+++ src/system/App.js
@@ -13,6 +13,8 @@ export class App {
 
         window.document.body.appendChild(this.app.view);
 
+        this.showLoadingPrompt(screenWidth / 2, screenHeight / 2);
+
         // Load assets
         // TODO: Split sprites by load priority & load some of them in background
         await this.loadAssets();
@@ -25,5 +27,27 @@ export class App {
     async loadAssets() {
         await PIXI.Assets.init({ manifest: 'assets/manifest.json' });
         await PIXI.Assets.loadBundle(['common', 'menu', 'stairs', 'decorations']);
+    }
+
+    showLoadingPrompt(xPosition, yPosition) {
+        const style = new PIXI.TextStyle({
+            fontFamily: 'Arial',
+            fontSize: 56,
+            fontStyle: 'italic',
+            fontWeight: 'bold',
+            fill: ['#a9f625', '#4ab416'],
+            stroke: '#dddddd',
+            strokeThickness: 2,
+            wordWrap: true,
+            wordWrapWidth: 440,
+            lineJoin: 'round',
+        });
+
+        const richText = new PIXI.Text('Loading...', style);
+        richText.anchor.set(0.5, 0.5);
+        richText.x = xPosition;
+        richText.y = yPosition;
+
+        this.app.stage.addChild(richText);
     }
 }
